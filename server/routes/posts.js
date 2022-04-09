26bbab2c2d3f4718423477b10f5cd562317cb59c
ceddadh <cedanik1@gmail.com>
2022-04-09 15:07:06 +0200

    initial

---
+++ server/routes/posts.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+  getPostsBySearch,
+  getPost,
+  postComment,
+} from "../controllers/posts.js";
+import auth from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/search", getPostsBySearch);
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", auth, createPost);
+router.post("/:id/commentPost", auth, postComment);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
+
+export default router;
