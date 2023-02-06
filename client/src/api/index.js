c5111322b26c7d47d2be959a9557bbcb4c808ccb
ceddah <cedanik1@gmail.com>
2023-02-06 20:07:44 +0100

    update path

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "/api" });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token}`;
